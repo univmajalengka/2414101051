183812bc02e0aa96bedc9851eb3a71e51238a124
irfaatinr21 <irfaatinr@gmail.com>
2025-12-18 10:24:14 +0700

    Delete tugas_capstone directory

--- tugas_capstone/hapus_pesanan.php
+++
@@ -1,8 +0,0 @@
-<?php
-include 'koneksi.php';
-$id = (int)($_GET['id'] ?? 0);
-if ($id) {
-    mysqli_query($koneksi, "DELETE FROM pesanan WHERE id = $id");
-}
-header("Location: daftar_pesanan.php");
-?>
